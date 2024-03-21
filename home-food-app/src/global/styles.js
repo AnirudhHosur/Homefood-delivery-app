e4791dc67f14a5e835becce081b9f8b976bd15a2
Anirudh <an516658@dal.ca>
2024-03-20 23:58:07 -0600

    Pushing first screen

---
+++ home-food-app/src/global/styles.js
@@ -0,0 +1,17 @@
+// colors in global level
+export const colors = {
+    buttons: '#ff8c52',
+    grey1: '#43484d',
+    grey2: '#5e6977',
+    grey3: '#86939e',
+    grey4: '#bdc6cf',
+    grey5: '#e1e8ee',
+    CardComment: '#86939e',
+    cardBackground: 'white',
+    statusbar: '#ff8c52',
+    headerText: 'white'
+}
+
+export const parameters = {
+    headerHeight: 40,
+}
