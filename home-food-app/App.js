3823dce2cf692dacb6753453e8dd306e6253b6ab
Anirudh <an516658@dal.ca>
2024-03-27 00:53:37 -0600

    Added welcome screen

--- home-food-app/App.js
+++ home-food-app/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, StatusBar } from "react-native";
 import { colors } from "./src/global/styles";
 import SignInScreen from "./src/screens/authScreens/SignInScreen";
+import SignInWelcomeScreen from "./src/screens/authScreens/SignInWelcomeScreen";
 
 export default function App() {
   return(
@@ -10,8 +11,9 @@ export default function App() {
         barStyle="light-content"
         backgroundColor = {colors.statusbar}
       />
+      <SignInWelcomeScreen />
 
-      <SignInScreen />
+      {/* <SignInScreen /> */}
     </View>
   )
 }
