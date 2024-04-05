b4986eb58760be1b461e5ad4bad46716e5334d09
Anirudh <an516658@dal.ca>
2024-04-04 22:18:01 -0600

    Developed contents in the MenuDrawer

--- home-food-app/src/navigation/DrawerNavigator.js
+++ home-food-app/src/navigation/DrawerNavigator.js
@@ -4,12 +4,15 @@ import { View, Text, StyleSheet, Dimensions, Image } from "react-native";
 import RootClientTabs from "./ClientTabs";
 import { Icon } from "react-native-elements";
 import { colors } from "../global/styles";
+import BusinessConsoleScreen from "../screens/BusinessConsoleScreen";
+import DrawerContent from "../components/DrawerContent";
 
 const Drawer = createDrawerNavigator()
 
 export default function DrawerNavigator() {
     return(
         <Drawer.Navigator
+        drawerContent={props => <DrawerContent {...props} />}
             screenOptions = {{
                 //activeTintColor: colors.buttons,
                 headerShown: false
@@ -24,6 +27,23 @@ export default function DrawerNavigator() {
                     drawerIcon: ({focussed, size}) => (
                         <Icon 
                             name="home"
+                            type="material"
+                            color={focussed ? '#7cc' : colors.grey2}
+                            size={size}
+                        />
+                    )
+                }}
+            />
+
+            <Drawer.Screen 
+                name="BusinessConsoleScreen"
+                component={BusinessConsoleScreen}
+
+                options={{
+                    title: 'Business Console',
+                    drawerIcon: ({focussed, size}) => (
+                        <Icon 
+                            name="business"
                             type="material"
                             color={focussed ? '#7cc' : colors.grey2}
                             size={size}
